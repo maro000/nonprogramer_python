e84cf43d8a7a6e3c056e54a9ffdee5eb678060a5
hacktoberman <56116261+hacktoberman@users.noreply.github.com>
2019-10-03 07:30:56 -0400

    "Falase" > "False" and pep8 function names

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,4 +1,4 @@
-def is_Phone_Number(text):
+def is_phone_number(text):
     if len(text) != 12:
         return False
     for i in range(0, 3):
@@ -13,16 +13,16 @@ def is_Phone_Number(text):
         return False
     for i in range(8, 12):
         if not text[i].isdecimal():
-            return Falase
+            return False
     return True
 
 
 print('Phone Number is 411-321-3211')
-print(is_Phone_Number("411-321-3211"))
+print(is_phone_number("411-321-3211"))
 
 message = '明日415-555-1011に電話してください。オフィスは415-555-9999です。'
 for i in range(len(message)):
     chunk = message[i:i + 12]  # ❶
-    if is_Phone_Number(chunk):  # ❷
+    if is_phone_number(chunk):  # ❷
         print('電話番号が見つかりました: ' + chunk)
 print('完了')
